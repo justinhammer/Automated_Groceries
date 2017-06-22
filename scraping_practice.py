8686608829d8a597214a1e76933a50044eebe088
Justin Hammer <justhamr@gmail.com>
2017-06-21 22:02:36 -0600

    Continued work on site navigation. Outline is pretty much finished. Now just need to take outline and convert it to loops that visit every page needed.

--- scraping_practice.py
+++ scraping_practice.py
@@ -9,11 +9,13 @@ from selenium.webdriver.common.keys import Keys
 import pdb
 #pdb.set_trace()
 
-website = 'https://www.smithsfoodanddrug.com/onlineshopping/signin'
+
 login_email = credentials.EMAIL_ADDRESS
 login_password = credentials.PASSWORD
 login_form_email_address_id = 'emailAddress'
 login_form_password_id = 'password'
+website = 'https://www.smithsfoodanddrug.com/onlineshopping/signin'
+webiste_home = "https://www.smithsfoodanddrug.com/storecatalog/clicklistbeta/#/"
 zip_code = credentials.ZIP_CODE
 
 departments_children_left_side_selector = '#categoryList_column1 > li'
@@ -21,9 +23,14 @@ departments_children_right_side_selector = '#categoryList_column2 > li'
 departments_drop_down_selector = '#departmentsButton > ng-transclude > div > span.desktop.label'
 departments_parent_left_side_id = 'categoryList_column1'
 departments_parent_right_side_id = 'categoryList_column2'
+home_link_selector = '#widget_breadcrumb > ul > li:nth-child(1) > a'
+next_page_arrow_id = 'WC_SearchBasedNavigationResults_pagination_link_right_categoryResults'
 search_bar_selector = '[data-qa="pickup store search input"]'
 search_button_selector = '[data-qa="pickup store submit button"]'
 shop_this_store_button_selector = 'ol > li:nth-child(2) > input'
+sort_by_drop_down_id = "products_sort_by"
+sort_by_alphabetical_selector = '[value="name"]'
+
 
 # Driver setup and opening browser:
 driver = webdriver.Chrome()
@@ -43,6 +50,8 @@ login_password_field.submit()
 shop_this_store_button = driver.find_element_by_css_selector(shop_this_store_button_selector)
 shop_this_store_button.click()
 
+
+    
 # Click the Departments drop down at the top of the page
 departments_drop_down = driver.find_element_by_css_selector(departments_drop_down_selector)
 departments_drop_down.click()
@@ -56,8 +65,24 @@ departments_parent_right_side = driver.find_element_by_id(departments_parent_rig
 departments_children_right_side_list = departments_parent_right_side.find_elements_by_css_selector(
     departments_children_right_side_selector)
 
+# Click the first item on the left side of the departments list
+wait = WebDriverWait(driver, 10)
+first_department_in_list = wait.until(EC.visibility_of(departments_children_left_side_list[0]))    
+first_department_in_list.click()
 
+# Sort the page alphabetically since relevance could change anytime
+sort_by_drop_down = driver.find_element_by_id(sort_by_drop_down_id)
+sort_by_drop_down.click()
+sort_by_alphabetical = driver.find_element_by_css_selector(sort_by_alphabetical_selector)
+sort_by_alphabetical.click()
 
+# Click the next pagination arrow at the bottom of the page
+next_page_arrow = driver.find_element_by_id(next_page_arrow_id)
+next_page_arrow.click()
+
+# Click the home page link
+home_link = driver.find_element_by_css_selector(home_link_selector)
+home_link.click()
 
 #   
 #   -For each of the links:
