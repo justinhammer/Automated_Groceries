7faac0cfef76ffab079f4fdfd712eb0a9e3510fc
Justin Hammer <justhamr@gmail.com>
2017-06-26 22:53:41 -0600

    Just starting to get database set up. I am certain that this will change at some point in the future as I figure out exactly all of the functionality I want in my app.

--- Automated_Groceries/main/models.py
+++ Automated_Groceries/main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    unit_of_measurement = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def price(self):
+        return "$%s" % self.price
