92ea5d836f364e49c5c6a2cc6b9bcc4354f25736
Justin Hammer <justhamr@gmail.com>
2017-06-26 23:48:28 -0600

    Slight changes to models

--- Automated_Groceries/main/models.py
+++ Automated_Groceries/main/models.py
@@ -3,13 +3,9 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=255)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2, default=0)
     unit_of_measurement = models.CharField(max_length=20)
-    upc = models.CharField(max_length=25)
+    upc = models.CharField(max_length=25, default='DEFAULT')
 
     def __str__(self):
         return self.name
-
-    @property
-    def price(self):
-        return "$%s" % self.price
